17fae19eb8927fd24c4444d6917a4b5dde67e650
Abdul karim Yassin <karim.mmd12@gmail.com>
2021-03-08 11:57:19 +0100

    setup dev dependencies

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import expenses from './reducers/expenses';
-// import filters from './reducers/filters';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import AppRouter from './routers/AppRouter';
 
 const store = configureStore();
-console.log('testing');
 const jsx = (
     <Provider store={store}>
         <AppRouter />
