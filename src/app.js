88f735316c1876c72eef80b8db74f6eac419576e
Abdul karim Yassin <karim.mmd12@gmail.com>
2021-03-29 22:29:29 +0200

    setting expenses from database

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/configureStore';
-import { Provider } from 'react-redux';
-import 'normalize.css/normalize.css';
-import './styles/style.scss';
-import AppRouter from './routers/AppRouter';
-import './firebase/firebase.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import configureStore from './store/configureStore'
+import { Provider } from 'react-redux'
+import { startSetExpenses } from './actions/expenses'
+import 'normalize.css/normalize.css'
+import './styles/style.scss'
+import AppRouter from './routers/AppRouter'
+import './firebase/firebase.js'
 
-const store = configureStore();
+const store = configureStore()
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-);
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
 
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'))
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('root'))
+})
